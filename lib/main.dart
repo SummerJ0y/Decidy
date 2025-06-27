ed65a33e2e22180e25ef6c4adc22ef6105f7bdf9
Yingjia Liu <yliu4033@gmail.com>
2025-06-27 14:23:44 -0500

    press animation and ripple effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './pages/home_page.dart';
+import './pages/result_page.dart';
+import 'app_state.dart'; 
 
 void main() {
   runApp(DecidyApp());
@@ -23,112 +25,3 @@ class DecidyApp extends StatelessWidget {
   }
 }
 
-class DecidyState extends ChangeNotifier {
-  String spokenText = '我要不要吃炸鸡';
-  String decisionResult = '当然要！炸鸡不等人！';
-
-  void clear() {
-    spokenText = '';
-    decisionResult = '';
-    notifyListeners();
-  }
-}
-
-// lib/home_page.dart
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final appState = context.watch<DecidyState>();
-
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onLongPressStart: (_) {
-            // TODO: Start speech recognition
-            print('🎤 开始录音');
-          },
-          onLongPressEnd: (_) {
-            // TODO: Stop speech and navigate
-            print('🛑 停止录音，准备跳转');
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                transitionDuration: Duration(milliseconds: 400), // 更缓一些
-                pageBuilder: (_, __, ___) => ResultPage(),
-                transitionsBuilder: (_, animation, __, child) {
-                  return FadeTransition(
-                    opacity: CurvedAnimation(
-                      parent: animation,
-                      curve: Curves.easeOutCubic, // 更自然的“空气中浮现”感
-                    ),
-                    child: child,
-                  );
-                },
-              ),
-            );
-          },
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepPurple, Colors.purpleAccent],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 12,
-                  offset: Offset(0, 4),
-                ),
-              ],
-            ),
-            child: Icon(Icons.mic, size: 40, color: Colors.white),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// lib/result_page.dart
-class ResultPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final appState = context.watch<DecidyState>();
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Decidy 回答')),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('你刚刚说的是：', style: TextStyle(fontSize: 16)),
-            SizedBox(height: 8),
-            Text(
-              appState.spokenText,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 32),
-            Text('Decidy 说：', style: TextStyle(fontSize: 16)),
-            SizedBox(height: 8),
-            Text(appState.decisionResult, style: TextStyle(fontSize: 22)),
-            Spacer(),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  appState.clear();
-                  Navigator.pop(context);
-                },
-                child: Text('再来一个'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,59 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../app_state.dart';
 import 'result_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  bool _isPressed = false;
+  late AnimationController _glowController;
+
+  @override
+  void initState() {
+    super.initState();
+    _glowController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  @override
+  void dispose() {
+    _glowController.dispose();
+    super.dispose();
+  }
+
+  void _onPressStart() {
+    setState(() => _isPressed = true);
+    _glowController.repeat(reverse: true);
+    HapticFeedback.mediumImpact();
+    print('🎤 开始录音');
+  }
+
+  void _onPressEnd() {
+    setState(() => _isPressed = false);
+    _glowController.stop();
+    print('🛑 停止录音，准备跳转');
+
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration(milliseconds: 400),
+        pageBuilder: (_, __, ___) => ResultPage(),
+        transitionsBuilder: (_, animation, __, child) {
+          return FadeTransition(
+            opacity: CurvedAnimation(
+              parent: animation,
+              curve: Curves.easeOutCubic,
+            ),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final appState = context.watch<DecidyState>();
-
     return Scaffold(
       body: Center(
         child: GestureDetector(
-          onLongPressStart: (_) {
-            // TODO: Start speech recognition
-            print('🎤 开始录音');
-          },
-          onLongPressEnd: (_) {
-            // TODO: Stop speech and navigate
-            print('🛑 停止录音，准备跳转');
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                transitionDuration: Duration(milliseconds: 400), // 更缓一些
-                pageBuilder: (_, __, ___) => ResultPage(),
-                transitionsBuilder: (_, animation, __, child) {
-                  return FadeTransition(
-                    opacity: CurvedAnimation(
-                      parent: animation,
-                      curve: Curves.easeOutCubic, // 更自然的“空气中浮现”感
+          onLongPressStart: (_) => _onPressStart(),
+          onLongPressEnd: (_) => _onPressEnd(),
+          onTapDown: (_) => setState(() => _isPressed = true),
+          onTapUp: (_) => setState(() => _isPressed = false),
+          onTapCancel: () => setState(() => _isPressed = false),
+          child: AnimatedOpacity(
+            duration: Duration(milliseconds: 100),
+            opacity: _isPressed ? 0.7 : 1.0,
+            child: AnimatedScale(
+              duration: Duration(milliseconds: 100),
+              scale: _isPressed ? 0.9 : 1.0,
+              curve: Curves.easeOut,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  // Glowing ripple
+                  if (_isPressed)
+                    AnimatedBuilder(
+                      animation: _glowController,
+                      builder: (_, __) {
+                        final glow = Tween(begin: 0.0, end: 60.0).animate(_glowController);
+                        final opacity = Tween(begin: 0.5, end: 0.0).animate(_glowController);
+                        return Container(
+                          width: 200 + glow.value,
+                          height: 200 + glow.value,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.deepPurple.withOpacity(opacity.value),
+                          ),
+                        );
+                      },
                     ),
-                    child: child,
-                  );
-                },
+
+                  // Mic button
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.deepPurple, Colors.purpleAccent],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 12,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Icon(Icons.mic, size: 40, color: Colors.white),
+                  ),
+                ],
               ),
-            );
-          },
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepPurple, Colors.purpleAccent],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 12,
-                  offset: Offset(0, 4),
-                ),
-              ],
             ),
-            child: Icon(Icons.mic, size: 40, color: Colors.white),
           ),
         ),
       ),
